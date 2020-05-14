5e73404f13ce2e13c84f7cbdbb95485a47fcb1cf
DaniswaraNoorFauziWicaksana <mrx1945.m1@gmail.com>
2020-05-14 08:52:53 +0700

    Praktikum Mobile 6

---
+++ app/src/main/java/com/example/modul6/konfigurasi.java
@@ -0,0 +1,22 @@
+package com.example.modul6;
+
+public class konfigurasi {
+    public static final String URL_ADD="http://192.168.0.54/android/mahasiswa/insert.php";
+    public static final String URL_GET_ALL="http://192.168.0.54/android/mahasiswa/read.php";
+    public static final String URL_GET_MHS="http://192.168.0.54/android/mahasiswa/select.php?id=";
+    public static final String URL_UPDATE_MHS="http://192.168.0.54/android/mahasiswa/update.php";
+    public static final String URL_DELETE_MHS="http://192.168.0.54/android/mahasiswa/delete.php?id=";
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+
+    public static final String MHS_ID = "mhs_id";
+}
